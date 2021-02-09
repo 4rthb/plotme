cf4810c8ec1ea49f40543469316d78079a9a056b
Lucas Balbinot <lucasbalbinot03@gmail.com>
2021-02-08 22:58:08 -0300

    Class fixes and defineAxis function

--- plotMe.py
+++ plotMe.py
@@ -4,9 +4,50 @@ import pandas as pd
 import argparse
 import re
 
-class plot:
+class Plot:
 
-    def __init__(self):
+    def __init__(self, 
+                fileName='', 
+                setPalette='colorblind', 
+                graphType='line',
+                output= '.pdf', 
+                x=0, 
+                y='1', 
+                symbols='ball', 
+                distBetSymbols=None, 
+                symbolSize=None, 
+                figSize=None, 
+                fontSize=None, 
+                legendFontSize=None,
+                lineWidth=None,
+                plotTitle=None,
+                xLabel=None,
+                yLabel=None,
+                cmd=False ):
+        if cmd:
+            # if it is called by command line
+            self.parseCmd()
+        else:
+            # if it is called by another program
+            self.fileName = fileName
+            self.setPalette = setPalette 
+            self.graphType = graphType
+            self.output = output 
+            self.x = x 
+            self.y = y 
+            self.symbols = symbols 
+            self.distBetSymbols = distBetSymbols 
+            self.symbolSize = symbolSize
+            self.figSize = figSize
+            self.fontSize= fontSize
+            self.legendFontSize=legendFontSize
+            self.lineWidth=lineWidth
+            self.plotTitle=plotTitle
+            self.xLabel=xLabel
+            self.yLabel=yLabel
+
+
+    def parseCmd(self):
         #parses arguments
         self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="""I can plot 4 types of graphs: Bar, Line, Pie and Scatter""")
         self.parser.add_argument("-f","--fileName", help="Name of the file that will provide data for the graph: name.extension", required=True)
@@ -14,8 +55,8 @@ class plot:
         self.parser.add_argument("-g","--graphType", help="Type of graph that will be plotted", default="line", choices=['line', 'pie', 'bar', 'scatter'])
         self.parser.add_argument("-o","--output", help="Name and/or extension of the desired output file", default=".pdf")
         self.parser.add_argument("-x", "--x", help="The abscissa of the graph", default=0)
-        self.parser.add_argument("-y", "--y", help="The ordinate(s) of the graph", default=(1))
-        self.parser.add_argument("-s", "--symbols", help="Shape of the symbols used", default=("ball"))
+        self.parser.add_argument("-y", "--y", help="The ordinate(s) of the graph", default='1')
+        self.parser.add_argument("-s", "--symbols", help="Shape of the symbols used", default=("ball" ,))
         self.parser.add_argument("-d", "--distBetSymbols", help="Distance between each symbol", default=None)
         self.parser.add_argument("-ss", "--symbolSize", help="Size of each symbol", default=None)
         self.parser.add_argument("-fig", "--figSize", help="Size of the graph", default=None)
@@ -25,46 +66,116 @@ class plot:
         self.parser.add_argument("-plot", "--plotTitle", help="Title that appears at the top of the graph", default=None)
         self.parser.add_argument("-xl", "--xLabel", help="Label of the abscissa", default=None)
         self.parser.add_argument("-yl", "--yLabel", help="Label of the ordinate(s)", default=None)
-        self.args = self.parser.parse_args()
+        
+        # and put the values in the class variables
+        args = self.parser.parse_args()
+        self.fileName = args.fileName
+        self.setPalette = args.setPalette 
+        self.graphType = args.graphType
+        self.output = args.output 
+        self.x = args.x 
+        self.y = args.y 
+        self.symbols = args.symbols 
+        self.distBetSymbols = args.distBetSymbols 
+        self.symbolSize = args.symbolSize
+        self.figSize = args.figSize
+        self.fontSize = args.fontSize
+        self.legendFontSize = args.legendFontSize
+        self.lineWidth = args.lineWidth
+        self.plotTitle = args.plotTitle
+        self.xLabel = args.xLabel
+        self.yLabel = args.yLabel
 
-    def fileWise(self):
-        outName = self.args.output
-        fName = self.args.fileName
 
-        def exportFile(outName,fName):
-            pattern1 = re.compile('^\.\w+')
-            pattern2 = re.compile('^.+\.\w+')
+    def importFile(self):
+        fileName = self.fileName
+        print(fileName)
+        
+        if type(fileName) == str:
+            sep=' '
+            if ',' in fileName:
+                sep = ','
+            
+            fileName = fileName.split(sep)
 
-            if pattern1.match(outName):
-                #only the extension
-                if pattern2.match(fName):
-                    #checks if the fileName contains an extension
-                    [fName, ext] = fName.split('.')
-
-                outName=outName.replace(".","")
-
-                if re.search("^(eps|jpeg|jpg|pdf|pgf|png|ps|raw|rgba|svg|svgz|tif|tiff)",outName):
-                    #search if given extension is supported
-                    plt.savefig(fName+'Plot.'+outName, bbox_inches="tight")
-                else:
-                    print("Extension not supported")
-
-            elif pattern2.match(outName):
-                #extension and name
-                [outName, ext] = outName.split('.')
-                if re.search("^(eps|jpeg|jpg|pdf|pgf|png|ps|raw|rgba|svg|svgz|tif|tiff)",ext):
-                    #search if given extension is supported
-                    plt.savefig(outName+'.'+ext, bbox_inches="tight")
-                else:
-                    print("Extension not supported")
-
+        handlers = []
+        for files in fileName:
+            name, ext = files.split('.')
+            
+            if ext:
+                # opens a single file
+                pass
             else:
-                plt.savefig(outName+'.pdf', bbox_inches="tight")
+                # opens a full directory
+                pass
+        
+
+    def defineAxis(self):
+        y = self.y
+        #splits y in every comma, to get every interval/value separated
+        y = y.split(',')
+
+        # the set that will store the values
+        # the data structure is a set to make sure that there will be no repeated values
+        values = set()
+
+        for interval in y:
+            # for every value in the list, check if it is an interval or a single value
+            val = interval.split('-')
+            
+            # if it's a list, then it's an interval
+            if len(val) > 1:
+                # gets the limits
+                val = [int(x) for x in val]
+                low_limit = min(val)
+                high_limit = max(val)
+                # adds each value
+                for k in range(low_limit, high_limit):
+                    values.add(k)
+                continue
+
+            #otherwise, it's a single value and just add it to the set
+            values.add(int(val[0]))
+        
+        # transform to a list
+        values = list(values)
+        # sort the values to get them in order
+        values.sort()
+
+        self.y = values
 
 
+    def exportFile(self,outName,fName):
+        pattern1 = re.compile('^\.\w+')
+        pattern2 = re.compile('^.+\.\w+')
 
-        exportFile(outName,fName)
+        if pattern1.match(outName):
+            #only the extension
+            if pattern2.match(fName):
+                #checks if the fileName contains an extension
+                [fName, ext] = fName.split('.')
+
+            outName=outName.replace(".","")
+
+            if re.search("^(eps|jpeg|jpg|pdf|pgf|png|ps|raw|rgba|svg|svgz|tif|tiff)",outName):
+                #search if given extension is supported
+                plt.savefig(fName+'Plot.'+outName, bbox_inches="tight")
+            else:
+                print("Extension not supported")
+
+        elif pattern2.match(outName):
+            #extension and name
+            [outName, ext] = outName.split('.')
+            if re.search("^(eps|jpeg|jpg|pdf|pgf|png|ps|raw|rgba|svg|svgz|tif|tiff)",ext):
+                #search if given extension is supported
+                plt.savefig(outName+'.'+ext, bbox_inches="tight")
+            else:
+                print("Extension not supported")
+
+        else:
+            plt.savefig(outName+'.pdf', bbox_inches="tight")
+
 
 if __name__ == "__main__":
-    instance = plot()
-    print(instance.fileWise())
+    instance = Plot(cmd=True)
+    instance.importFile()
